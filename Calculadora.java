e652f5e7c2b30bc4b029207b1144a7ae98237f47
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2016-09-28 10:43:23 -0300

    Soporte para la resta

--- Calculadora.java
+++ Calculadora.java
@@ -7,7 +7,12 @@ public class Calculadora{
 			   int op1 = Integer.parseInt(args[1]);
 			   int op2 = Integer.parseInt(args[2]);
 			   
+			   if("+".equals(args[0]))
 			   System.out.println(op1 + op2);
+			   else if("-".equals(args[0]))
+			   System.out.println(op1 - op2);
+			   else
+			   System.out.println(args[0] + "Comando desconocido");
 			   
 			}catch(Exception ex){
                System.out.println("Wrong parameters");
